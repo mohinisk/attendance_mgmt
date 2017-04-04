44301c1596513aa0d8b75fbd0f3a43a99edfab91
pooja <pysomvanshi@gmail.com>
2017-04-04 15:50:53 +0530

    [doctyoe added] Attedence Log

---
+++ attedence_system/customization/attendance/attendance.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+from frappe.utils import getdate, nowdate
+from frappe import _
+from frappe.model.document import Document
+from erpnext.hr.utils import set_employee_name
+
+
+# @frappe.whitelist()
+# def leave_status(employee, satus, from_date, to_date):
+# 	
