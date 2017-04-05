1a4696fc5e86c86b29d41dabdeeaca0a42bad416
pooja <pysomvanshi@gmail.com>
2017-04-05 16:32:10 +0530

    [Attendance] status change

--- attedence_system/customization/attendance/attendance.py
+++ attedence_system/customization/attendance/attendance.py
@@ -7,9 +7,19 @@ import frappe
 from frappe.utils import getdate, nowdate
 from frappe import _
 from frappe.model.document import Document
-from erpnext.hr.utils import set_employee_name
+# from erpnext.hr.utils import set_employee_name
 
-
-# @frappe.whitelist()
-# def leave_status(employee, satus, from_date, to_date):
-# 	
+def check_leave_records(self, method):
+		#this is done to check if employee applied has applied for half-day leave or leave
+		#while creating Attendance Log
+	if self.status == 'Present':
+		leave = frappe.db.sql("""select name,  half_day  from `tabLeave Application`
+			where employee = %s and %s between from_date and to_date and status = 'Approved'
+			and docstatus = 1""", (self.employee, self.att_date), as_dict=1)
+		
+		if leave:
+			if leave[0]["half_day"] == 1:
+				frappe.db.set(self, 'status', "Half Day")
+			else:
+				frappe.throw(_("Employee {0} was on leave on {1}. Cannot mark attendance.").format(self.employee,
+					self.att_date))
