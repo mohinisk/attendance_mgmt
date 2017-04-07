f66f93b265a90a48e50c35b868dd192937b28f07
user <mohini.k@indictranstech.com>
2017-04-07 17:00:40 +0530

    status in list view

--- attedence_system/api/api.py
+++ attedence_system/api/api.py
@@ -20,13 +20,13 @@ def create_attendance_record(mac,employee,att_date,time):
 					attendance_doc.employee = employee
 					attendance_doc.att_date = att_date
 					attendance_doc.save(ignore_permissions=True)
+					attendance_doc.submit()
 
 				attendance_log = frappe.new_doc("Attendance Log")
 				attendance_log.employee = employee
 				attendance_log.att_date = att_date
 				attendance_log.time = time
-				attendance_log.save(ignore_permissions=True)
-				#attendance_doc.submit()
+				attendance_log.save(ignore_permissions=True)		
 				#attendance_log.submit()
 				return "success"
 			except Exception, e:
@@ -61,7 +61,4 @@ def status_absent():
 		except Exception, e:
 			print frappe.get_traceback()
 			raise e
-
-
-	
 
