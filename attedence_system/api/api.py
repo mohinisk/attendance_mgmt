b02d8cee0e59f33719a96f7f4b78a2c8f434226b
user <mohini.k@indictranstech.com>
2017-04-15 16:15:35 +0530

    [fix]Changed POST to  GET method

--- attedence_system/api/api.py
+++ attedence_system/api/api.py
@@ -1,44 +1,36 @@
 from __future__ import unicode_literals
 import json
 import frappe
-from uuid import getnode as get_mac
-
 
 @frappe.whitelist(allow_guest=True)
-def create_attendance_record(mac,employee,att_date,time):
-	mac1 = get_mac()
-	print "MAC1",type(long(mac1))
-	print "MAC",type(long(mac))
-	if long(mac) == long(mac1):
-		emp_name = frappe.db.get_value("Employee",{"name":employee},"employee_name")
-		if emp_name :
-			try:
-				att_record = frappe.db.get_value("Attendance",{"att_date":att_date,"status":"Present","employee":employee},"name")
-				print "att_record",att_record
-				if not att_record :
-					attendance_doc = frappe.new_doc("Attendance")
-					attendance_doc.employee = employee
-					attendance_doc.att_date = att_date
-					attendance_doc.save(ignore_permissions=True)
-					attendance_doc.submit()
+def create_attendance_record(employee,att_date,time):
+	print "\n\n\n\n\n\n employee",employee
+	print "att_date",att_date
+	print "time",time
+	emp_name = frappe.db.get_value("Employee",{"name":employee},"employee_name")
+	if emp_name :
+		try:
+			att_record = frappe.db.get_value("Attendance",{"att_date":att_date,"status":"Present","employee":employee},"name")
+			if not att_record :
+				attendance_doc = frappe.new_doc("Attendance")
+				attendance_doc.employee = employee
+				attendance_doc.att_date = att_date
+				attendance_doc.save(ignore_permissions=True)
+				#attendance_doc.submit()
 
-				attendance_log = frappe.new_doc("Attendance Log")
-				attendance_log.employee = employee
-				attendance_log.att_date = att_date
-				attendance_log.time = time
-				attendance_log.save(ignore_permissions=True)		
-				#attendance_log.submit()
-				return "success"
-			except Exception, e:
-				print frappe.get_traceback()
-				raise e
-		else:
-			print "Invalid Employee ID"
-			return "Invalid Employee ID"
+			attendance_log = frappe.new_doc("Attendance Log")
+			attendance_log.employee = employee
+			attendance_log.att_date = att_date
+			attendance_log.time = time
+			attendance_log.save(ignore_permissions=True)
+			print "attendance_log.name",attendance_log.name
+			frappe.db.commit()
+			return "Welcome",frappe.db.get_value("Employee",{"name":employee},"employee_name")
+		except Exception, e:
+			raise e
 	else:
-
-		return "INVALID user"
-
+			return "Invalid Employee ID"
+	
 
 @frappe.whitelist(allow_guest=True)
 def status_absent():
